344d75fb6d23cefb03091bf1f0bac5080bed4355
Amiroffir <amirosoffir@gmail.com>
2023-03-04 11:14:37 +0200

    CORS Enabling

--- WepAPISample/Program.cs
+++ WepAPISample/Program.cs
@@ -6,7 +6,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+// Enable Cors
+builder.Services.AddCors(options =>
+{
+	options.AddDefaultPolicy(builder =>
+	{
+		builder.AllowAnyOrigin()
+			   .AllowAnyHeader()
+			   .AllowAnyMethod();
+	});
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +26,7 @@ if (app.Environment.IsDevelopment())
 }
 app.UseHttpsRedirection();
 app.UseRouting();
+app.UseCors(); // Enable cors
 app.UseAuthorization();
 
 //app.MapControllers();
